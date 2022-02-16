9a7d61dcc766e448c2915ced00461b2e00900542
Rakesh Shaw11 <rashaw11@in.ibm.com>
2022-02-16 23:49:11 +0530

    initial commit

---
+++ src/hello-again/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Let's get started.. Hell to React world!!</h2>
+      <p>Hey Hello World again</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/hello/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Let's get started.. Hell to React world!!</h2>
+    </div>
+  );
+}
+
+export default App;
